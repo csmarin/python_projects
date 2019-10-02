b54694ff5ed80a9ba30c861507624e53a5d273f7
Cosmin Marin <cosminm@Cosmins-MacBook-Pro.local>
2019-10-02 07:42:54 +0100

    authentication working, but always redirecting

--- spotify_app/spotify.py
+++ spotify_app/spotify.py
@@ -1,20 +1,29 @@
 import os
 import sys
-import json, spotipy, webbrowser
+import json
+import spotipy
+import webbrowser
 import spotipy.util as util
 from json.decoder import JSONDecodeError
 
-username = sys.argv(1)
+username = sys.argv[1]
 
 # https://open.spotify.com/user/cosminmarin
+
+#scope = 
 
 # Erase cache and prompt for user permissions
 try:
     token = util.prompt_for_user_token(username)
 except:
-    os.remove(f".cache=(username)")
-    token = util. prompt_for_user_token(username)
+    os.remove(f".cache-{username}")
+    token = util.prompt_for_user_token(username)
 
-#create Spotify object
+#create Spotify objectß
 spotipyObject = spotipy.Spotify(auth=token)
 
+user = spotipyObject.current_user()
+
+print(json.dumps(user, sort_keys=True, indent=4))
+
+displayName = user['display_name']
