df007c200c616ba0849cf2204df28eab5287a348
Cosmin Marin <cosmin.marin@gmail.com>
2020-03-04 00:01:01 +0000

    Exercise 17

---
+++ connector/learning_python/exercises.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+print("I am $€")
+"""
+Exercise 17
+"""
+# from sys import argv
+# from os.path import exists
+
+# script, from_file, to_file = argv
+
+# print("Copying from {} to {}".format(from_file, to_file))
+
+# in_data = open(from_file).read()
+
+# print("The input file is {} bytes long.".format(len(in_data)))
+
+# print("Does the output file exists? {}".format(exists(to_file)))
+# print("When ready, hit RETURN to continue.")
+# input(">>>")
+
+# out_file = open(to_file,'w')
+# out_file.write(in_data)
+
+# print("Allright, all done.");out_file.close()
+
+"""
+Exercise 18
+"""
+
